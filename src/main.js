347cfcf54c9b75c3fc22314cfbc88a831348b4aa
kelucen <lucenalarissa0@gmail.com>
2021-01-16 21:37:18 -0400

    vuetify and bootstrap-vue were added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(vuetify);
